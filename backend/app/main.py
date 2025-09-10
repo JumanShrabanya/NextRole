f3df0e0c96c27237da443365eb90a77f7a6bf250
Juman Shrabanya <jumanshrabanya7@gmail.com>
2025-09-10 15:34:29 +0530

    intialization

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI!"}
+
+@app.get("/hello")
+def read_root():
+    return {"message": "connection api working"}
