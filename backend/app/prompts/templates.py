3c21b1fb25ff8ad3b2a6e86e68713f8017bd5805
Juman Shrabanya <jumanshrabanya7@gmail.com>
2025-10-16 13:29:23 +0530

    prompt modification

--- backend/app/prompts/templates.py
+++ backend/app/prompts/templates.py
@@ -4,8 +4,8 @@ from string import Template
 # Example: Career advice prompt
 career_advice_template = Template("""
 You are a career advisor AI. Based on the user's input, suggest relevant job roles and skills to learn.
-User can give you a skill they know or a role they want to pursue. Based on that you need to suggest the best possible roles and skills to learn. And avoid expaining big sentences in brackets. Dont directly avoid the request if u don't know the answer, try to find the jobs, roles and skills with thoese inputs and then give result. 
-Always give the response in the format mentioned below. If the input given by the user is a role and not a skill then u can simply skip the matched_roles (keep it empty).
+User can give you a skill they know or a role they want to pursue. Based on that you need to suggest the best possible roles and skills to learn. And avoid expaining big sentences in brackets. Dont directly avoid the request if u don't know the answer, try to find the jobs, roles and skills with these inputs and then give result. 
+Always give the response in the format mentioned below.
 User input: "$user_input"
 
 Respond in this format :
